ddaee4aed0ae9a833f85e8d6cf9a781038ce8a69
gasharva <vorikazdinoel@gmail.com>
2026-09-18 13:59:49 +0300

    Carry time signature across SVG pages

--- Tools/SvgScoreBatch/Program.cs
+++ Tools/SvgScoreBatch/Program.cs
@@ -27,7 +27,7 @@ var pageSet = DiscoverPages(inputDirectory, baseName);
 if (pageSet.Pages.Count == 0)
 {
     Console.Error.WriteLine(
-        $"No SVG pages matching <name>-NNN.svg were found in {inputDirectory}");
+        $"No SVG pages matching <name>-N.svg were found in {inputDirectory}");
     return 2;
 }
 
@@ -66,6 +66,7 @@ if (!noBuild)
 }
 
 var pageCanonicals = new List<CanonicalNotation>();
+TimeSignature? activeTimeSignature = null;
 var pagesDirectory = Path.Combine(
     outputDirectory,
     "pages");
@@ -101,6 +102,15 @@ foreach (var page in pageSet.Pages)
         processArgs.Add(Path.GetFullPath(modelPath));
     }
 
+    if (activeTimeSignature is not null)
+    {
+        processArgs.Add("--initial-time");
+        processArgs.Add(
+            $"{activeTimeSignature.Beats}/{activeTimeSignature.BeatType}");
+        Console.WriteLine(
+            $"Inherited time: {activeTimeSignature.Beats}/{activeTimeSignature.BeatType}");
+    }
+
     var exitCode = await RunProcessAsync(
         "dotnet",
         processArgs,
@@ -124,8 +134,13 @@ foreach (var page in pageSet.Pages)
         return 3;
     }
 
-    pageCanonicals.Add(
-        CanonicalJson.Read(canonicalPath));
+    var pageCanonical = CanonicalJson.Read(
+        canonicalPath);
+    pageCanonicals.Add(pageCanonical);
+
+    activeTimeSignature = ResolveFinalTimeSignature(
+        pageCanonical,
+        activeTimeSignature);
 }
 
 Console.WriteLine();
@@ -221,7 +236,7 @@ static PageSet DiscoverPages(
     string? requestedBase)
 {
     var regex = new Regex(
-        @"^(?<base>.+)-(?<page>\d{3})\.svg$",
+        @"^(?<base>.+)-(?<page>\d+)\.svg$",
         RegexOptions.IgnoreCase
         | RegexOptions.CultureInvariant);
 
@@ -312,6 +327,27 @@ static PageSet ValidatePageSet(
     return new PageSet(
         baseName,
         ordered);
+}
+
+static TimeSignature? ResolveFinalTimeSignature(
+    CanonicalNotation page,
+    TimeSignature? inherited)
+{
+    var current = inherited;
+
+    foreach (var measure in page.Parts
+                 .FirstOrDefault()
+                 ?.Measures
+                 .OrderBy(measure => measure.Number)
+             ?? [])
+    {
+        if (measure.Attributes?.Time is { } time)
+        {
+            current = time;
+        }
+    }
+
+    return current;
 }
 
 static CanonicalNotation ReadCanonical(
@@ -416,8 +452,8 @@ static void PrintUsage()
         + "[--no-build] [--fail-on-diff]");
     Console.Error.WriteLine();
     Console.Error.WriteLine(
-        "SVG pages must be named <name>-NNN.svg, for example "
-        + "prelude-001.svg, prelude-002.svg.");
+        "SVG pages must be named <name>-N.svg, for example "
+        + "prelude-1.svg, prelude-2.svg.");
 }
 
 sealed record SvgPage(
